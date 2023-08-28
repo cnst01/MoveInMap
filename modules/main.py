7be93362c8781bf4b0cbe29d1192054350801e8a
cnst01 <cassionst@gmail.com>
2023-08-28 20:01:10 -0300

    totally functional, misses a direct goTo movement

--- modules/main.py
+++ modules/main.py
@@ -1,66 +1,77 @@
-import keyboard
-import time
+from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
+from mindstorms.control import wait_for_seconds, wait_until, Timer
+from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
+import math
 
 class myMap:
     def __init__ (self,start_pos):
         self.start_pos = start_pos
         pos = [self.start_pos[0], self.start_pos[1], self.start_pos[2]]
         self.points = [pos]
-    
+
     def addPoint(self,point):
         pos = [point[0], point[1], point[2]]
         if not pos in self.points:
-            self.points += [pos] 
-    
+            self.points += [pos]
+
 class Robot:
-    def __init__ (self, position = [0,0,90]):
+    def __init__ (self,motorE,motorD, position = [0,0,0]):
         self.position = position
-        self.map = myMap(self.position)    
+        self.map = myMap(self.position)
+        self.motors = MotorPair(motorE, motorD)
+        self.hub = MSHub()
+        self.hub.motion_sensor.reset_yaw_angle()
+
+    def pointTo(self, degrees, precision = 1):
+        dir = self.position[2]
+        if(dir < degrees):
+            while not self.hub.motion_sensor.get_yaw_angle() in range(degrees - precision, degrees + precision):
+                self.motors.start_tank(20,-20)
+            self.motors.stop()
+        else:
+            while not self.hub.motion_sensor.get_yaw_angle() in range(degrees - precision, degrees + precision):
+                self.motors.start_tank(-20,20)
+            self.motors.stop()
+        print('turning ' + str(degrees) + ' degrees')
+        self.position[2] = self.hub.motion_sensor.get_yaw_angle()
+        self.map.points[-1][2] = self.position[2]
 
     def moveX(self,q):
-        if q : 
+        if q:
+            if q > 0:
+                self.pointTo(90)
+            else:
+                self.pointTo(-90)
+            self.motors.move(abs(q),'cm', 0, 50)
             print('moving ' + str(q) + ' on X')
             self.position[0] += q
             self.map.addPoint(self.position)
+    
     def moveY(self,q):
-        if q : 
+        if q :
+            if q > 0:
+                self.pointTo(0)
+            else:
+                self.pointTo(178)
+            self.motors.move(abs(q),'cm', 0, 50)
             print('moving ' + str(q) + ' on Y')
             self.position[1] += q
             self.map.addPoint(self.position)
-    def Turn(self, dir):
-        if dir : 
-            print('turning ' + str(dir) + ' degrees')
-            self.position[2] += dir
-            self.map.addPoint(self.position)
     
-    def goTO(self,x,y,dir = 90):
+
+    def goTo(self,x,y):
         self.moveX(x - self.position[0])
         self.moveY(y - self.position[1])
-        self.Turn(dir - self.position[2])
 
 
-def ManualMovement(robot):
-    if keyboard.is_pressed('w'):
-        robot.moveY(10)
-        time.sleep(0.5)
-        return 1
-    if keyboard.is_pressed('a'):
-        robot.moveX(-10)
-        time.sleep(0.5)
-        return 1
-    if keyboard.is_pressed('s'):
-        robot.moveY(-10)
-        time.sleep(0.5)
-        return 1
-    if keyboard.is_pressed('d'):
-        robot.moveX(10)
-        time.sleep(0.5)
-        return 1
-    return 0 
 
 
-if __name__ == '__main__':
-    robo = Robot()
-    while not keyboard.is_pressed('space'):
-        if ManualMovement(robo):
-            print(robo.map.points)
+def main():
+    robo = Robot('F', 'B')
+    robo.goTo(20,30)
+    print(robo.map.points)
+    robo.goTo(0,-10)
+    print(robo.map.points)
+    robo.pointTo(90)
+
+main()
