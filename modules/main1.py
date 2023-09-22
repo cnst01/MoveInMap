049ce36d17b79b28f9a83d5ce5d9cd08b8a7c319
guguskatistareidelas <144486173+guguskatistareidelas@users.noreply.github.com>
2023-09-22 14:02:36 -0300

    Update main1.py

--- modules/main1.py
+++ modules/main1.py
@@ -3,6 +3,8 @@ from mindstorms.control import wait_for_seconds, wait_until, Timer
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 import math
 
+
+
 class myMap:
     def __init__ (self,start_pos):
         self.start_pos = start_pos
@@ -15,13 +17,14 @@ class myMap:
             self.points += [pos]
 
 class Robot:
+    def moveFree(w,x,y,z):
+        MotorPair("F","B").move(w,x,y,z)
     def __init__ (self,motorE,motorD, position = [0,0,0]):
         self.position = position
         self.map = myMap(self.position)
         self.motors = MotorPair(motorE, motorD)
         self.hub = MSHub()
         self.hub.motion_sensor.reset_yaw_angle()
-        self.force_sensor = ForceSensor('A')
 
     def pointTo(self, degrees, precision = 1):
         dir = self.position[2]
@@ -58,13 +61,15 @@ class Robot:
             print('moving ' + str(q) + ' on Y')
             self.position[1] += q
             self.map.addPoint(self.position)
-
-
-    def goTo(self,[x,y]):
+    def goTo(self,pos):
+        x = pos[0]
+        y = pos[1]
         self.moveX(x + (-1*self.position[0]))
-        self.moveY(y + (-1*self.position[0]))
-    def goTo2(self,[x,y]):
-        self.moveY(y + (-1*self.position[0]))
+        self.moveY(y + (-1*self.position[1]))
+    def goTo2(self,pos):
+        x = pos[0]
+        y = pos[1]
+        self.moveY(y + (-1*self.position[1]))
         self.moveX(x + (-1*self.position[0]))
 
 class HubController:
@@ -148,26 +153,25 @@ def fechargarra():
 def subrirRampa():
     return 1
 def main():
-    petroleo = []
-    animal_enfermo1 = [-7.4,-42.2]
-    animal_enfermo2 = [-5.5,15.5]
+    animal_enfermo1 = [-41.5,30.3]
+    animal_enfermo2 = [-6,15.5]
     animal_enfermo3 = [16.0,45.5]
     destino_enfermo1 = [102,-13]
     destino_enfermo2 = [88,-13]
     destino_enfermo3 = [95,-3.5]
-    animal_encalhado1 = [-91.7,-32.0]
-    animal_encalhado2 = [-51.0,-19.0]
-    animal_encalhado3 = [-7.4,-42.2]
+    animal_encalhado1 = [-92.7,-32.0]
+    animal_encalhado2 = [-52.0,-27.0]
+    animal_encalhado3 = [-7.4,-43.2]
     destino_encalhado1 = [-16,52.5]
     destino_encalhado2 = [10.5, 52.5]
     destino_encalhado3 = [37.2,52.5]
     petroleo = [-94.0,45.9]
     frenterampa = [57,11]
     robo = Robot('F', 'B')
-    control = HubController('F','B')
-
-    alinhar()
-    #animais encalhados
+    #control = HubController('F','B')
+'''
+    #alinhar()
+    animais encalhados
     robo.goTo(0,0)
     abrirgarra()
     robo.goTo(animal_encalhado1)
@@ -204,14 +208,6 @@ def main():
     #trocargarra
     robo.goTo(frenteRampa)
     subirRampa()
-    
-    #petroleo
-    robo.goTo(petroleo)
-    fechargarra()
-    robo.goTo(0,0)
-    robo.goTo(frenteRampa)
-    subirRampa()
-    abrirGarra()
-
-
-main()     
+    robo.pointTo(135)
+'''
+main()
